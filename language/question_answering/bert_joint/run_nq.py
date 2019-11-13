354afa6c2b17de75a6d0dfa0f59ec19495a61ecb
Hono Shirai <h.shirai@datumstudio.jp>
2019-11-13 16:04:58 +0900

    Fix unsupport function for python3

--- language/question_answering/bert_joint/run_nq.py
+++ language/question_answering/bert_joint/run_nq.py
@@ -34,6 +34,8 @@ import tensorflow as tf
 from tensorflow.contrib import cluster_resolver as contrib_cluster_resolver
 from tensorflow.contrib import tpu as contrib_tpu
 
+from six.moves import xrange
+
 flags = tf.flags
 FLAGS = flags.FLAGS
 
@@ -774,7 +776,7 @@ class CreateTFExampleFn(object):
             [input_feature.answer_type])
       else:
         token_map = [-1] * len(input_feature.input_ids)
-        for k, v in input_feature.token_to_orig_map.iteritems():
+        for k, v in input_feature.token_to_orig_map.items():
           token_map[k] = v
         features["token_map"] = create_int_feature(token_map)
 
@@ -1089,7 +1091,7 @@ class FeatureWriter(object):
       features["answer_types"] = create_int_feature([feature.answer_type])
     else:
       token_map = [-1] * len(feature.input_ids)
-      for k, v in feature.token_to_orig_map.iteritems():
+      for k, v in feature.token_to_orig_map.items():
         token_map[k] = v
       features["token_map"] = create_int_feature(token_map)
 
@@ -1160,7 +1162,7 @@ def compute_predictions(example):
   n_best_size = 10
   max_answer_length = 30
 
-  for unique_id, result in example.results.iteritems():
+  for unique_id, result in example.results.items():
     if unique_id not in example.features:
       raise ValueError("No feature found with unique_id:", unique_id)
     token_map = example.features[unique_id]["token_map"].int64_list.value
@@ -1397,7 +1399,7 @@ def main(_):
     tf.logging.info("  Num orig examples = %d", len(eval_examples))
     tf.logging.info("  Num split examples = %d", len(eval_features))
     tf.logging.info("  Batch size = %d", FLAGS.predict_batch_size)
-    for spans, ids in num_spans_to_ids.iteritems():
+    for spans, ids in num_spans_to_ids.items():
       tf.logging.info("  Num split into %d = %d", spans, len(ids))
 
     predict_input_fn = input_fn_builder(
@@ -1430,7 +1432,7 @@ def main(_):
     ]
     nq_pred_dict = compute_pred_dict(candidates_dict, eval_features,
                                      [r._asdict() for r in all_results])
-    predictions_json = {"predictions": nq_pred_dict.values()}
+    predictions_json = {"predictions": list(nq_pred_dict.values())}
     with tf.gfile.Open(FLAGS.output_prediction_file, "w") as f:
       json.dump(predictions_json, f, indent=4)
 
